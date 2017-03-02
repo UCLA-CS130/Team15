9993a6e7a97ad6f30bd3256d4c0a5472594766df
Thomas <thomas.chang7@gmail.com>
2017-03-02 01:51:12 -0800

    Add integration test for proxy redirect

--- integration_test.sh
+++ integration_test.sh
@@ -28,6 +28,10 @@ path / StaticHandler {
   root /foo/bar;
 }
 path /echo EchoHandler{}
+path /80 ProxyHandler {
+  server_host ucla.edu;
+  server_port 80;
+}
 path /2020 ProxyHandler {
   server_host localhost;
   server_port 2020;
@@ -53,6 +57,11 @@ curl -i -s localhost:8080/2020/echo > test_proxy_response
 DIFF_PROXY=$(diff expected_proxy_response test_proxy_response)
 EXIT_STATUS_PROXY=$?
 
+curl -i -s localhost:8080/80/ > test_proxy_redirect_response
+head -n 1 test_proxy_redirect_response > test_proxy_redirect_response1
+DIFF_REDIRECT=$(diff test_proxy_redirect_response1 expected_proxy_redirect_response)
+EXIT_STATUS_PROXY_REDIRECT=$?
+
 # Error handling
 if [ "$EXIT_STATUS" -eq 0 ]
 then
@@ -73,11 +82,24 @@ else
     echo $DIFF_PROXY
 fi
 
+#Proxy Redirected Handling
+if [ "$EXIT_STATUS_PROXY_REDIRECT" -eq 0 ]
+then
+    echo "SUCCESS: Proxy 302 test passed"
+else
+    echo "FAILED: Proxy 302 test failed"
+    echo "diff: "
+    echo $DIFF_REDIRECT
+fi
+
+
 # Shutdown the webserver and cleanup
 echo "Cleaning up and shutting down"
 pkill webserver
 make clean
 rm -f test_response
 rm -f test_proxy_response
+rm -f test_proxy_redirect_response
+rm -f test_proxy_redirect_response1
 
 exit "$EXIT_STATUS"
